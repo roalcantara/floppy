283b761586f4f25cb8dc78b59999e5a0e4b4211c
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2023-05-10 00:39:40 -0300

    feat(unity): Add PlayModeTests
    Which can run as a standalone in a Player or inside the Editor.
    
    More: https://docs.unity3d.com/Packages/com.unity.test-framework@1.0/manual/edit-mode-vs-play-mode-tests.html

---
+++ src/Assets/Tests/PlayModeTests/PlayModeExampleTests.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PlayModeExampleTests
+{
+  // A Test behaves as an ordinary method
+  [Test]
+  public void PlayModeExampleTestsSimplePasses()
+  {
+    // Use the Assert class to test conditions
+    Assert.True(true);
+  }
+
+  // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+  // `yield return null;` to skip a frame.
+  [UnityTest]
+  public IEnumerator PlayModeExampleTestsWithEnumeratorPasses()
+  {
+    // Use the Assert class to test conditions.
+    // Use yield to skip a frame.
+    Assert.True(true);
+    yield return null;
+    Assert.True(true);
+  }
+}
